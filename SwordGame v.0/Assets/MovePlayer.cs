d90551ed249de90ff12eb443b107a77a344abec6
FortuneCookie07 <fortuneplays999@gmail.com>
2023-05-09 16:08:05 -0500

    Movement Implementation v2

--- SwordGame v.0/Assets/MovePlayer.cs
+++ SwordGame v.0/Assets/MovePlayer.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    public float speed = 1f; 
+    public float speed = 5f; 
     public Rigidbody rb;
-    public Vector3 movement;
+    //public Vector3 movement;
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +20,17 @@ public class MovePlayer : MonoBehaviour
         // In Update we get the Input for left, right, up and down and put it in the variable 'movement'...
         // We only get the input of x and z, y is left at 0 as it's not required
         // 'Normalized' diagonals to prevent faster movement when two inputs are used together
-        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        //movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
     }
 
     void FixedUpdate()
     {
         // We call the function 'moveCharacter' in FixedUpdate for Physics movement
-        rb.velocity = direction * speed * Time.deltaTime;
+        //rb.velocity = direction * speed * Time.deltaTime;
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        rb.MovePosition(transform.position + movement * Time.deltaTime * speed);
     }
 
     
