c96a9fd0d84eaa663421b1ca8bc502442ba7f979
FortuneCookie07 <fortuneplays999@gmail.com>
2023-05-17 20:08:19 -0500

    Menu Screen v3
    Added first draft menu screen buttons and transitions. Note to self: add back buttons from each panel

---
+++ SwordGame v.0/Assets/Scripts/UITransitionManager.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class UITransitionManager : MonoBehaviour
+{
+    public CinemachineVirtualCamera currentCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentCamera.Priority++;
+    }
+
+    // Update is called once per frame
+    public void UpdateCamera(CinemachineVirtualCamera target)
+    {
+        currentCamera.Priority--;
+
+        currentCamera = target;
+
+        currentCamera.Priority++;
+    }
+}
